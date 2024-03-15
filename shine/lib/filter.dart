b29635bd0e7554df714c877b43aa806a6ae3bc98
samiyamohammed <121340936+samiyamohammed@users.noreply.github.com>
2024-03-15 23:15:02 +0300

    filter.dart

--- shine/lib/filter.dart
+++ shine/lib/filter.dart
@@ -1,288 +1,216 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
-class FilterPage extends StatefulWidget {
-  @override
-  _FilterPageState createState() => _FilterPageState();
+void main() {
+  runApp(const ProfileApp());
 }
 
-class _FilterPageState extends State<FilterPage> {
-  double hueValue = 0.0;
-  double saturationValue = 0.0;
-  double contrastValue = 0.0;
-  double brightnessValue = 0.0;
-
-  String selectedGender = 'All';
-  String selectedBrand = 'Adidas';
-  RangeValues selectedPriceRange = RangeValues(0, 100);
-  List<String> selectedColors = [];
-
-  List<String> genders = ['All', 'Men', 'Women'];
-  List<String> brands = ['Adidas', 'Puma', 'CR7', 'Nike', 'Yeezy', 'Supreme'];
-  List<String> colors = ['White', 'Black', 'Grey', 'Yellow', 'Red', 'Green'];
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TextStyle customTextStyle = TextStyle(
-      fontFamily: 'Poppins',
-      fontSize: 15.0,
-      letterSpacing: 0.25,
+    return MaterialApp(
+      home: ProfilePage(),
     );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Filter',
-            style: customTextStyle.copyWith(
-                fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-        ),
-        leading: IconButton(
-          icon: Container(
-            width: 40.0,
-            height: 40.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.grey[200],
-            ),
-            child: Center(
-              child: Icon(
-                Icons.arrow_back,
-                color: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 40.0,
+              backgroundImage: NetworkImage(
+                'https://i.imgur.com/removed.png',
               ),
             ),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.only(left: 10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Gender',
-                    style:
-                        customTextStyle.copyWith(fontWeight: FontWeight.bold)),
-                SizedBox(height: 20),
-                Wrap(
-                  spacing: 3.0, // spacing between columns
-                  runSpacing: 10.0, // spacing between rows
-                  children: genders.map((String color) {
-                    bool isSelected = selectedColors.contains(color);
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if (isSelected) {
-                            selectedColors.remove(color);
-                          } else {
-                            selectedColors.add(color);
-                          }
-                        });
-                      },
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Container(
-                          width: 130,
-                          height: 50,
-                          color:
-                              isSelected ? Color(0xFF6055D8) : Colors.grey[200],
-                          child: Center(
-                            child: Text(
-                              color,
-                              style: customTextStyle.copyWith(
-                                color: isSelected
-                                    ? Colors.white
-                                    : Colors.grey[600],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-                SizedBox(height: 20),
-                Text('Brand',
-                    style:
-                        customTextStyle.copyWith(fontWeight: FontWeight.bold)),
-                SizedBox(height: 20),
-                Wrap(
-                  spacing: 3.0, // spacing between columns
-                  runSpacing: 10.0, // spacing between rows
-                  children: brands.map((String color) {
-                    bool isSelected = selectedColors.contains(color);
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if (isSelected) {
-                            selectedColors.remove(color);
-                          } else {
-                            selectedColors.add(color);
-                          }
-                        });
-                      },
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Container(
-                          width: 130,
-                          height: 50,
-                          color:
-                              isSelected ? Color(0xFF6055D8) : Colors.grey[200],
-                          child: Center(
-                            child: Text(
-                              color,
-                              style: customTextStyle.copyWith(
-                                color: isSelected
-                                    ? Colors.white
-                                    : Colors.grey[600],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-                SizedBox(height: 20),
-                Text('Price Range',
-                    style:
-                        customTextStyle.copyWith(fontWeight: FontWeight.bold)),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '\$16',
-                        style:
-                            customTextStyle.copyWith(color: Colors.grey[700]),
-                      ),
-                      Text(
-                        '\$453',
-                        style:
-                            customTextStyle.copyWith(color: Colors.grey[700]),
-                      ),
-                    ],
+            const SizedBox(height: 20.0),
+            const Text(
+              'Mark Adam',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            const Text(
+              'Sunny_Koelpin45@hotmail.com',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            const NavigationButton(
+              text: 'Profile',
+              icon: Icons.person,
+              showArrowIcon: true,
+            ),
+            const NavigationButton(
+              text: 'Settings',
+              icon: Icons.settings,
+              showArrowIcon: true,
+            ),
+            const NavigationButton(
+              text: 'Contact',
+              icon: Icons.mail,
+              showArrowIcon: true,
+            ),
+            const NavigationButton(
+              text: 'Share App',
+              icon: Icons.share,
+              showArrowIcon: true,
+            ),
+            const NavigationButton(
+              text: 'Help',
+              icon: Icons.help,
+              showArrowIcon: true,
+            ),
+            const Spacer(),
+            Align(
+              alignment: Alignment.center,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Sign Out',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                RangeSlider(
-                  values: selectedPriceRange,
-                  min: 0,
-                  max: 100,
-                  onChanged: (RangeValues values) {
-                    setState(() {
-                      selectedPriceRange = values;
-                    });
+              ),
+            ),
+            const SizedBox(height: 20.0), // Add spacing between navigation buttons and bottom buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                BottomIconButton(
+                  icon: Icons.home,
+                  isActive: false,
+                  onPressed: () {
+                    // Navigate to home screen
                   },
                 ),
-                SizedBox(height: 20),
-                Text('Color',
-                    style:
-                        customTextStyle.copyWith(fontWeight: FontWeight.bold)),
-                SizedBox(height: 20),
-                Wrap(
-                  spacing: 3.0,
-                  runSpacing: 10.0,
-                  children: colors.map((String color) {
-                    bool isSelected = selectedColors.contains(color);
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if (isSelected) {
-                            selectedColors.remove(color);
-                          } else {
-                            selectedColors.add(color);
-                          }
-                        });
-                      },
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Container(
-                          width: 130,
-                          height: 50,
-                          color:
-                              isSelected ? Color(0xFF6055D8) : Colors.grey[200],
-                          child: Center(
-                            child: Text(
-                              color,
-                              style: customTextStyle.copyWith(
-                                color: isSelected
-                                    ? Colors.white
-                                    : Colors.grey[600],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
+                BottomIconButton(
+                  icon: Icons.search,
+                  isActive: false,
+                  onPressed: () {
+                    // Navigate to search screen
+                  },
                 ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      print('Another options tapped');
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      child: ListTile(
-                        title: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Another options',
-                              style: customTextStyle.copyWith(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Icon(
-                              Icons.navigate_next,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                BottomIconButton(
+                  icon: Icons.shopping_bag,
+                  isActive: false,
+                  onPressed: () {
+                    // Navigate to bag screen
+                  },
+                ),
+                BottomIconButton(
+                  icon: Icons.person,
+                  isActive: true,
+                  onPressed: () {
+                    // Navigate to profile screen
+                  },
                 ),
               ],
             ),
-          ),
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          onPressed: () {
-            // Apply Filters logic here
-          },
-          child: Text(
-            'Apply Filters',
-            style: customTextStyle.copyWith(
-              color: Colors.white,
-            ),
-          ),
-          style: ButtonStyle(
-            backgroundColor:
-                MaterialStateProperty.all<Color>(Color(0xFF6055D8)),
-            minimumSize:
-                MaterialStateProperty.all<Size>(Size(double.infinity, 50)),
-          ),
+          ],
         ),
       ),
     );
   }
 }
 
-void main() {
-  runApp(MaterialApp(
-    home: FilterPage(),
-  ));
+class BottomIconButton extends StatelessWidget {
+  final IconData icon;
+  final bool isActive;
+  final VoidCallback onPressed;
+
+  const BottomIconButton({
+    Key? key,
+    required this.icon,
+    required this.isActive,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Icon(
+        icon,
+        color: isActive ? Colors.blue : Colors.grey,
+      ),
+    );
+  }
+}
+class NavigationButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final bool showArrowIcon;
+  final bool isSignOutButton;
+
+  const NavigationButton({
+    Key? key,
+    required this.text,
+    required this.icon,
+    this.showArrowIcon = false,
+    this.isSignOutButton = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: sized_box_for_whitespace
+    return Container(
+      height: 48.0,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          alignment: Alignment.centerLeft,
+          foregroundColor: Colors.black,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 24.0,
+                  height: 24.0,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  child: Icon(
+                    icon,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(width: 10.0),
+                Text(
+                  text,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: isSignOutButton
+                        ? Colors.red
+                        : const Color.fromARGB(255, 5, 5, 5),
+                  ),
+                ),
+              ],
+            ),
+            if (showArrowIcon) const Icon(Icons.chevron_right),
+          ],
+        ),
+      ),
+    );
+  }
 }
